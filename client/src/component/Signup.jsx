79ec7ca07b26787e0b103e4ded895a4a8448a0c8
ashutosh-dgksocial <ashutoshdgksocial@gmail.com>
2024-07-03 19:06:35 +0530

    last commit

--- client/src/component/Signup.jsx
+++ client/src/component/Signup.jsx
@@ -2,17 +2,32 @@ import axios from 'axios';
 import { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
+import * as Yup from 'yup';
 
 const Signup = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   const navigate = useNavigate();
+  const [formData, setFormData] = useStaet({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, 'Name must be at least 3 characters long')
+      .required('Name is required'),
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters long')
+      .required('Password is required'),
+  });
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name === '' || email === '' || password === '') {
+    if (FormData.name === '' || FormData.email === '' || FormData.password === '') {
       alert("can't leave empty field")
       return;
     }
@@ -34,17 +49,17 @@ const Signup = () => {
             <Form.Group controlId="formBasicName">
               <Form.Label>Name</Form.Label>
               <Form.Control type="text" placeholder="Enter Name" name='name'
-                onChange={(e) => setName(e.target.value)} />
+                onChange={(e) => setFormData(e.target.value)} />
             </Form.Group>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" name='email'
-                onChange={(e) => setEmail(e.target.value)} />
+                onChange={(e) => setFormData(e.target.value)} />
             </Form.Group>
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" name='password'
-                onChange={(e) => setPassword(e.target.value)} />
+                onChange={(e) => setFormData(e.target.value)} />
             </Form.Group>
             <Form.Group controlId="formBasicConfirmPassword">
               <Form.Label>Confirm Password</Form.Label>
